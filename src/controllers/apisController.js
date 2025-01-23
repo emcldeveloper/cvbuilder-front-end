afdadf8466859a7602feb4ea5788b76616144dc9
Halidi Selemani <developer@exactmanpower.co.tz>
2025-01-23 15:07:41 +0300

    update main cv builder

--- src/controllers/apisController.js
+++ src/controllers/apisController.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export const checkIfExists = async ({ uuid }) => {
     try {
         // Replace the URL with your Laravel API endpoint
-        const response = await axios.get(`http://127.0.0.1:8000/api/cv/cv_builder/${uuid}`);
+        const response = await axios.get(`https://ekazi.co.tz/api/cv/cv_builder/${uuid}`);
        
         // Assuming the Laravel API returns a JSON response with a boolean `exists` field
         if (response.data.exists) {
