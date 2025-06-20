22421559bda61f9471f3d7f1f0667fa6abc9782b
kelvinmushy <kevo93mushy@gmail.com>
2025-06-20 11:29:39 +0300

    fix:data

--- src/Auth/RequireVerification.js
+++ src/Auth/RequireVerification.js
@@ -1,18 +1,16 @@
-// components/RequireVerification.jsx
-import React from 'react';
-import { Navigate, useLocation } from 'react-router-dom';
 
-const RequireVerification = ({ children }) => {
+import React from 'react';
+import { Navigate, useLocation, Outlet } from 'react-router-dom';
+
+const RequireVerification = () => {
   const location = useLocation();
   const isVerified = localStorage.getItem('verified') === '1';
 
   if (!isVerified) {
-    // Redirect to the not-verified page
     return <Navigate to="/not-verified" state={{ from: location }} replace />;
   }
 
-  // If verified, render the child components
-  return children;
+  return <Outlet />; 
 };
 
 export default RequireVerification;
